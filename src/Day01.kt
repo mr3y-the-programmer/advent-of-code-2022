51eb0802cb3c002f1358009b5a2eec49bca660b2
MR3Y <abdonasr379@gmail.com>
2022-12-01 12:04:50 +0200

    Add solution for Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,33 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Int? {
+        return input
+            .split { line -> line.isBlank() }
+            .maxOfOrNull { elfCalories -> elfCalories.sumOf { it.toInt() } }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .split { line -> line.isBlank() }
+            .map { elfCalories -> elfCalories.sumOf { it.toInt() } }
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println(part1(readInput("Day01_input")))
+    println(part2(readInput("Day01_input")))
+}
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+private fun List<String>.split(predicate: (String) -> Boolean): List<List<String>> {
+    val transformed = mutableListOf<List<String>>()
+    var temp = mutableListOf<String>()
+    forEach {
+        if (!predicate(it)) {
+            temp.add(it)
+        } else {
+            transformed.add(temp)
+            temp = mutableListOf()
+        }
+    }
+    return transformed
 }
